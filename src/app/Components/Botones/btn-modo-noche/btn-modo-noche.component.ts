2e253750c32e0d580d24d3230a37bb892c6bc70d
juanesfa210907 <juanesfa210907@gmail.com>
2024-11-20 17:42:54 +0000

    commit

--- src/app/Components/Botones/btn-modo-noche/btn-modo-noche.component.ts
+++ src/app/Components/Botones/btn-modo-noche/btn-modo-noche.component.ts
@@ -3,10 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-btn-modo-noche',
   standalone: true,
-  imports: [],
   templateUrl: './btn-modo-noche.component.html',
-  styleUrl: './btn-modo-noche.component.css'
+  styleUrls: ['./btn-modo-noche.component.css']
 })
 export class BtnModoNocheComponent {
+  isDarkMode: boolean = false; // Estado del modo oscuro
 
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    document.body.classList.toggle('dark-mode', this.isDarkMode);
+  }
 }
